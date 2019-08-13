47a71465b37bb5e02150c8b5787f971abe128253
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-08-13 16:09:27 -0400

    backend files set up. Ready for routes

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const mongoose = require('mongoose');
 //====== CONFIGURATION ======
 const port = 3000;
 
-//  ========== CONNECT TO MONGOOSE ========== //
+app.use(express.json());
+
+//========== CONTROLLERS ==========//
+const travelsController = require('./controllers/travel.js');
+app.use('/traveltracker', travelsController);
+
+//========== CONNECT TO MONGOOSE ==========//
 mongoose.connect('mongodb://localhost:27017/traveltracker', {useNewUrlParser: true});
 mongoose.connection.once('open', () => {
   console.log('connected to mongoose...');
