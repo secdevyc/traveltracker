d730fe2141ef735e100a9b1a075a937473b1b028
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-08-13 18:34:49 -0400

    post route created and can add data to database

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,17 @@
 const app = angular.module('TravelApp', [])
 
-app.controller('MainController', function() {
+app.controller('MainController', ['$http', function($http) {
+  this.createForm = {};
 
-})
+  this.createTravel = () => {
+    $http({
+      method:'POST',
+      url:'/traveltracker',
+      data: this.createForm
+    }).then(response => {
+      console.log(response.data);
+    }, error => {
+      console.log(error);
+    })
+  }//end createTravel()
+}])// end controller
