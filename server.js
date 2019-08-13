ad5b492c97009c24818870be791780b90bc0695b
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-08-13 11:05:45 -0400

    installed mongoose and required it in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //======== DEPENDENCIES ========
 const express = require('express')
 const app = express();
+const mongoose = require('mongoose');
 //====== CONFIGURATION ======
 const port = 3000;
 
