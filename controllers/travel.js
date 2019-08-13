c394adaedfe78f19bad85bcf75f8c7ac8afa02c5
Yulli Chong <yc@Yullis-MacBook-Pro.local>
2019-08-13 15:16:06 -0500

    fixed schema name on travel.js controller

--- controllers/travel.js
+++ controllers/travel.js
@@ -5,27 +5,27 @@ const Travels = require('../models/travel.js');
 // ========== ROUTES ========== //
 //========= INDEX GET ROUTE ============
 router.get('/', (req, res) => {
-  TRAVELS.find({}, (err, foundEntries) => {
+  Travels.find({}, (err, foundEntries) => {
     res.json(foundEntries)
   })
 })
 
 //=========== DELETE ROUTE ==============
 router.delete('/:id', (req, res) => {
-  TRAVELS.findByIdAndRemove(req.params.id, (err, deletedEntry) => {
+  Travels.findByIdAndRemove(req.params.id, (err, deletedEntry) => {
     res.json(deletedEntry)
   })
 })
 
 //=========== EDIT ROUTE =================
 router.put('/:id', (req, res) => {
-  TRAVELS.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedEntry) => {
+  Travels.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedEntry) => {
     res.json(updatedTodo)
   })
 })
 //============ CREATE POST ROUTE ==========
 router.post('/', (req, res) => {
-  TRAVELS.create(req.body, (err, createdEntry) => {
+  Travels.create(req.body, (err, createdEntry) => {
     res.json(createdEntry);
   })
 })
