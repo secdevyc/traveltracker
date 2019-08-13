8e02c99d9fe52b500240c9e04c3f48c41ff55334
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-08-13 16:03:16 -0400

    created Travels schema

---
+++ models/travel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const travelSchema = mongoose.Schema({
+  location: String,
+  date: String,
+  pros: String,
+  cons: String
+});
+
+const Travels = mongoose.model('Travels', travelSchema);
+
+module.exports = Travels;
