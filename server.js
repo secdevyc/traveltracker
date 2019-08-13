b34dc53ac9e4c4f694474ad482b9ea33666bfb4b
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-08-13 11:07:58 -0400

    connected to mongoose in server.js

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ const mongoose = require('mongoose');
 //====== CONFIGURATION ======
 const port = 3000;
 
+//  ========== CONNECT TO MONGOOSE ========== //
+mongoose.connect('mongodb://localhost:27017/traveltracker', {useNewUrlParser: true});
+mongoose.connection.once('open', () => {
+  console.log('connected to mongoose...');
+})
+
 //======= ROUTES ===========
 app.get('/', (req, res) => {
   res.send('HELLO WORLD')
