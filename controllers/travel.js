6cd46ace7fb44daa3b85898ff24e3ef67871096a
Alex Hawkins <alexhawkins@Alexs-MacBook-Pro.local>
2019-08-13 10:57:13 -0400

    set up controllers/travel.js file

---
+++ controllers/travel.js
@@ -0,0 +1,9 @@
+conste express = require('express');
+const router = express.Router();
+//connect model here
+
+// ========== ROUTES ========== //
+
+
+// ========== EXPORT ========== //
+module.exports = router;
